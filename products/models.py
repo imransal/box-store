7c48563ab641fc9f29c9ce33ee68daa13145a275
imransal <imrannasirsaleem@gmail.com>
2023-09-08 14:24:34 +0000

    customized admin

--- products/models.py
+++ products/models.py
@@ -1,17 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 
 class Category(models.Model):
-    """
-    Category Model
-    """
 
     class Meta:
         verbose_name_plural = 'Categories'
-
+        
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, blank=True)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -21,13 +17,8 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    """
-    Product Model
-    """
-
-    category = models.ForeignKey(
-        'Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, blank=True)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
