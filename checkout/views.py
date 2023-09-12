146cec34b793fd949a3d0a8fb70bbc6f8bef956d
imransal <imrannasirsaleem@gmail.com>
2023-09-12 19:45:49 +0000

    reviewing code

--- checkout/views.py
+++ checkout/views.py
@@ -27,6 +27,7 @@ def cache_checkout_data(request):
             processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
 
+
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
@@ -47,7 +48,11 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
@@ -104,7 +109,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51J5GrWBRf7ddZ5NdfI7hNauw1AdK3CTQKu5HelWAfzx0Q4qTXwbHwUvaos1k6zYo4FsYi3vgoW1EgouFZk9VB79u002HRxSZ8V',
+        'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
     }
 
