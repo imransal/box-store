bbb8db93029f96f90814ecdda5a4dbdc520e749a
imransal <imrannasirsaleem@gmail.com>
2023-09-15 09:33:48 +0000

    updated allowed hosts in settings.py

--- box_store/settings.py
+++ box_store/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['box-store.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['box-store.herokuapp.com', 'localhost', 'box-store-90aa6734c1b0.herokuapp.com']
 
 
 # Application definition
