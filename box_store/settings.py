ec173d27da398a60059cbbb73871739ba15bf0b9
imransal <imrannasirsaleem@gmail.com>
2023-09-16 17:28:36 +0000

    update settings

--- box_store/settings.py
+++ box_store/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', ' ')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['box-store-90aa6734c1b0.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['box-store-90aa6734c1b0.herokuapp.com', 'localhost', 'https://8000-imransal-boxstore-sl9dxr1y0u0.ws-eu104.gitpod.io/', '*']
 
 
 # Application definition
